b295bc1cf27e390af1637c20044e3d76972bf132
lisitsinyr <lisitsinyr@gmail.com>
2024-02-16 18:36:00 +0400

    20240216183559

--- BAT/lyrgit_config.bat
+++ BAT/lyrgit_config.bat
@@ -52,54 +52,52 @@ rem     --[no-]default <value>
 rem                           with --get, use default value when missing entry
 rem ----------------------------------------------------------------------------
 chcp 1251>NUL
+setlocal enableextensions disabledelayedexpansion
+
+rem SCRIPT_FULLFILENAME - Файл скрипта [каталог+имя+расширение]
+set SCRIPT_FULLFILENAME=%0
+rem echo SCRIPT_FULLFILENAME: %SCRIPT_FULLFILENAME%
 
 echo ==================================================================
-echo SET %0 ...
+echo SET %SCRIPT_FULLFILENAME% ...
 echo ==================================================================
-setlocal enableextensions disabledelayedexpansion
-rem CURRENT_DIR - Текущий каталог
-set CURRENT_DIR=%CD%
-set DIR_SAVE=%CURRENT_DIR%
-set SCRIPTS_DIR=D:\TOOLS\TOOLS_BAT
 call :__SET_SCRIPTS_DIR || exit /b 1
-call :__SET_VAR_SCRIPT %0 || exit /b 1
+call :__SET_VAR_SCRIPT %SCRIPT_FULLFILENAME% || exit /b 1
 call :__SET_VAR_DEFAULT || exit /b 1
 call :__SET_VAR_PROJECTS || exit /b 1
 call :__SET_CHECK_REPO || exit /b 1
 call :__SET_LOG || exit /b 1
 
+:begin
 call :StartLogFile || exit /b 1
 
-:begin
 echo ================================================================= >> %LOG_FULLFILENAME%
 echo START %SCRIPT_BASEFILENAME% ... >> %LOG_FULLFILENAME%
 echo ================================================================== >> %LOG_FULLFILENAME%
+set DIR_SAVE=%CURRENT_DIR%
 
 rem BODY script ..............................................
-call :PATTERN || exit /b 1
-echo --------------------------------------------------------------- > %LOG_FULLFILENAME%
-echo ...git config --list --show-origin --show-scope >> %LOG_FULLFILENAME%
-echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
-git config --list --show-origin --show-scope  >> %LOG_FULLFILENAME%
+
+call :BODY || exit /b 1
+
 rem BODY script ..............................................
 
 echo ================================================================= >> %LOG_FULLFILENAME%
 echo STOP %SCRIPT_BASEFILENAME% ... >> %LOG_FULLFILENAME%
 echo ================================================================== >> %LOG_FULLFILENAME%
+
 cd /D %DIR_SAVE%
 rem far -v %LOG_FULLFILENAME%
 
 call :StopLogFile || exit /b 1
+
 rem Выход из сценария. Дальше - только функции.
 :Exit
 exit /b 0
 
 rem =================================================
-rem ФУНКЦИИ
+rem ФУНКЦИИ LIB
 rem =================================================
-:__SET_SCRIPTS_DIR
-%SCRIPTS_DIR%\LIB\__SET_LIB.bat %*
-exit /b 0
 :__SET_VAR_SCRIPT
 %SCRIPTS_DIR%\LIB\__SET_LIB.bat %*
 :__SET_VAR_DEFAULT
@@ -114,19 +112,41 @@ exit /b 0
 :__SET_LOG
 %SCRIPTS_DIR%\LIB\__SET_LIB.bat %*
 exit /b 0
-
 :StartLogFile
 %SCRIPTS_DIR%\LIB\LYRLog.bat %*
 exit /b 0
 :StopLogFile
 %SCRIPTS_DIR%\LIB\LYRLog.bat %*
 exit /b 0
+:Check_P
+%SCRIPTS_DIR%\LIB\LYRSupport.bat %*
+exit /b 0
+rem =================================================
+rem ФУНКЦИЯ :__SET_SCRIPTS_DIR
+rem =================================================
+:__SET_SCRIPTS_DIR
+rem beginfunction
+    rem echo ---------------------------------------------------------------
+    rem echo __SET_SCRIPTS_DIR
+    rem echo ---------------------------------------------------------------
+    rem SCRIPTS_DIR - Каталог скриптов
+    if "%SCRIPTS_DIR%" == "" (
+        set SCRIPTS_DIR=D:\TOOLS\TOOLS_BAT
+        set SCRIPTS_DIR=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\TOOLS_BAT
+        set SCRIPTS_DIR=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\PROJECTS_BAT\TOOLS_BAT
+    )
+    rem echo SCRIPTS_DIR: %SCRIPTS_DIR%
+    exit /b 0
+rem endfunction
 
 rem =================================================
-rem ФУНКЦИИ BODY
+rem ФУНКЦИЯ :BODY
 rem =================================================
-:PATTERN
-echo ---------------------------
-echo 1.PATTERN:
-echo ---------------------------
-exit /b 0
+:BODY
+rem beginfunction
+    echo --------------------------------------------------------------- > %LOG_FULLFILENAME%
+    echo ...git config --list --show-origin --show-scope >> %LOG_FULLFILENAME%
+    echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+    git config --list --show-origin --show-scope  >> %LOG_FULLFILENAME%
+    exit /b 0
+rem endfunction
--- BAT/lyrgit_push_main.bat
+++ BAT/lyrgit_push_main.bat
@@ -40,67 +40,52 @@ rem     -4, --ipv4            use IPv4 addresses only
 rem     -6, --ipv6            use IPv6 addresses only
 rem -------------------------------------------------------------------
 chcp 1251>NUL
+setlocal enableextensions disabledelayedexpansion
+
+rem SCRIPT_FULLFILENAME - Файл скрипта [каталог+имя+расширение]
+set SCRIPT_FULLFILENAME=%0
+rem echo SCRIPT_FULLFILENAME: %SCRIPT_FULLFILENAME%
 
 echo ==================================================================
-echo SET %0 ...
+echo SET %SCRIPT_FULLFILENAME% ...
 echo ==================================================================
-setlocal enableextensions disabledelayedexpansion
-rem CURRENT_DIR - Текущий каталог
-set CURRENT_DIR=%CD%
-set DIR_SAVE=%CURRENT_DIR%
-set SCRIPTS_DIR=D:\TOOLS\TOOLS_BAT
 call :__SET_SCRIPTS_DIR || exit /b 1
-call :__SET_VAR_SCRIPT %0 || exit /b 1
+call :__SET_VAR_SCRIPT %SCRIPT_FULLFILENAME% || exit /b 1
 call :__SET_VAR_DEFAULT || exit /b 1
 call :__SET_VAR_PROJECTS || exit /b 1
 call :__SET_CHECK_REPO || exit /b 1
 call :__SET_LOG || exit /b 1
 
+:begin
 call :StartLogFile || exit /b 1
 
-:begin
 echo ================================================================= >> %LOG_FULLFILENAME%
 echo START %SCRIPT_BASEFILENAME% ... >> %LOG_FULLFILENAME%
 echo ================================================================== >> %LOG_FULLFILENAME%
+set DIR_SAVE=%CURRENT_DIR%
 
 rem BODY script ..............................................
-call :Check_P1 || exit /b 1
-echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
-echo ...git add --all >> %LOG_FULLFILENAME%
-echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
-git add --all >> %LOG_FULLFILENAME%
 
-echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
-echo ...git commit -m "%Comment%" >> %LOG_FULLFILENAME%
-echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
-git commit -m "%Comment%" >> %LOG_FULLFILENAME%
-rem git commit -m "%Comment%"
+call :BODY || exit /b 1
 
-echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
-echo ...git push -u origin main >> %LOG_FULLFILENAME%
-echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
-git push -u origin main >> %LOG_FULLFILENAME%
-rem git push -u origin main
-echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
 rem BODY script ..............................................
 
 echo ================================================================= >> %LOG_FULLFILENAME%
 echo STOP %SCRIPT_BASEFILENAME% ... >> %LOG_FULLFILENAME%
 echo ================================================================== >> %LOG_FULLFILENAME%
+
 cd /D %DIR_SAVE%
 rem far -v %LOG_FULLFILENAME%
 
 call :StopLogFile || exit /b 1
+
 rem Выход из сценария. Дальше - только функции.
 :Exit
 exit /b 0
 
 rem =================================================
-rem ФУНКЦИИ
+rem ФУНКЦИИ LIB
 rem =================================================
-:__SET_SCRIPTS_DIR
-%SCRIPTS_DIR%\LIB\__SET_LIB.bat %*
-exit /b 0
 :__SET_VAR_SCRIPT
 %SCRIPTS_DIR%\LIB\__SET_LIB.bat %*
 :__SET_VAR_DEFAULT
@@ -115,29 +100,57 @@ exit /b 0
 :__SET_LOG
 %SCRIPTS_DIR%\LIB\__SET_LIB.bat %*
 exit /b 0
-
 :StartLogFile
 %SCRIPTS_DIR%\LIB\LYRLog.bat %*
 exit /b 0
 :StopLogFile
 %SCRIPTS_DIR%\LIB\LYRLog.bat %*
 exit /b 0
+:Check_P
+%SCRIPTS_DIR%\LIB\LYRSupport.bat %*
+exit /b 0
+rem =================================================
+rem ФУНКЦИЯ :__SET_SCRIPTS_DIR
+rem =================================================
+:__SET_SCRIPTS_DIR
+rem beginfunction
+    rem echo ---------------------------------------------------------------
+    rem echo __SET_SCRIPTS_DIR
+    rem echo ---------------------------------------------------------------
+    rem SCRIPTS_DIR - Каталог скриптов
+    if "%SCRIPTS_DIR%" == "" (
+        set SCRIPTS_DIR=D:\TOOLS\TOOLS_BAT
+        set SCRIPTS_DIR=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\TOOLS_BAT
+        set SCRIPTS_DIR=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\PROJECTS_BAT\TOOLS_BAT
+    )
+    rem echo SCRIPTS_DIR: %SCRIPTS_DIR%
+    exit /b 0
+rem endfunction
 
 rem =================================================
-rem ФУНКЦИИ BODY
+rem ФУНКЦИЯ :BODY
 rem =================================================
-:Check_P1
-echo ---------------------------------------------------------------
-echo Check_P1 [Comment]
-echo ---------------------------------------------------------------
-if "%1"=="" (
-    rem set /p Comment=Comment:
+:BODY
+rem beginfunction
+    set Comment="Git Bash commit update"
     set Comment=%date:~6,4%%date:~3,2%%date:~0,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
-) else (
-    set Comment=%1
-)
-if "%Comment%"=="" (
-    set Comment=Git Bash commit update
-)
-echo Comment: %Comment%
-exit /b 0
+    call :Check_P Comment %Comment% || exit /b 1
+    echo Comment: %Comment%
+
+    echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+    echo ...git add --all >> %LOG_FULLFILENAME%
+    echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+    git add --all >> %LOG_FULLFILENAME%
+    
+    echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+    echo ...git commit -m "%Comment%" >> %LOG_FULLFILENAME%
+    echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+    git commit -m "%Comment%" >> %LOG_FULLFILENAME%
+
+    echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+    echo ...git push -u origin main >> %LOG_FULLFILENAME%
+    echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+    git push -u origin main >> %LOG_FULLFILENAME%
+    echo --------------------------------------------------------------- >> %LOG_FULLFILENAME%
+    exit /b 0
+rem endfunction
